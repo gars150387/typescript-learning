e2b5f2100271d7c099d9b702266e654446a54234
Gustavo <gar.santeliz@gmail.com>
2022-08-26 21:40:49 -0400

    data type

---
+++ src/App.js
@@ -0,0 +1,8 @@
+"use strict";
+const msg = 'Hola mundo!!!!';
+console.log(msg);
+const hero = {
+    name: 'gust',
+    age: 35,
+};
+hero.age = 54;
---
+++ src/App.ts
@@ -0,0 +1,14 @@
+
+
+
+const msg = 'Hola mundo!!!!'
+
+console.log(msg)
+
+
+const hero ={
+    name: 'gust',
+    age: 35,
+}
+
+hero.age = 54
